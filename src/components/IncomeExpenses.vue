1d1ed8c9c9bc6fb44d4b9a28b1cb40580beafd3c
Luke Sutton <lukesutton@me.com>
2023-12-23 16:20:37 +0000

    Add components and configurations for a VueJS expense tracker
    Basic application built using Vue3 and the composition API

---
+++ src/components/IncomeExpenses.vue
@@ -0,0 +1,59 @@
+<script setup>
+defineProps({
+  income: {
+    type: Number,
+    required: true
+  },
+  expenses: {
+    type: Number,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="inc-exp-container">
+    <div>
+      <h4>Income</h4>
+      <p id="money-plus" class="money plus">+${{ income }}</p>
+    </div>
+    <div>
+      <h4>Expense</h4>
+      <p id="money-minus" class="money minus">${{ expenses }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.money {
+  font-size: 20px;
+  letter-spacing: 1px;
+  margin: 5px 0;
+}
+
+.money.plus {
+  color: #2ecc71;
+}
+
+.money.minus {
+  color: #c0392b;
+}
+
+.inc-exp-container {
+  background-color: #fff;
+  box-shadow: var(--box-shadow);
+  padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  margin: 20px 0;
+}
+
+.inc-exp-container > div {
+  flex: 1;
+  text-align: center;
+}
+
+.inc-exp-container > div:first-of-type {
+  border-right: 1px solid #dedede;
+}
+</style>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,36 @@
+<script setup>
+const emit = defineEmits(['transactionDeleted'])
+
+defineProps({
+  transactions: {
+    type: Array,
+    required: true
+  }
+})
+
+const deleteTransaction = (id) => {
+  emit('transactionDeleted', id)
+}
+</script>
+
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li
+      v-for="transaction in transactions"
+      :key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }} <span>${{ transaction.amount }}</span
+      ><button @click="deleteTransaction(transaction.id)" class="delete-btn">x</button>
+    </li>
+    <!--    <li class="minus">-->
+    <!--      Cash <span>-$400</span><button class="delete-btn">x</button>-->
+    <!--    </li>-->
+    <!--    <li class="plus">-->
+    <!--      Paycheck <span>$800</span><button class="delete-btn">x</button>-->
+    <!--    </li>-->
+  </ul>
+</template>
+
+<style scoped></style>
